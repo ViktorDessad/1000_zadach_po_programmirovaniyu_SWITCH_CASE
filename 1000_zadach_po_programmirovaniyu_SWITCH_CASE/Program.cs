a6356974cd83d5f2e12a80f81d11d46947643b05
Віктор Шереш <sheregi1000@gmail.com>
2019-05-13 21:32:00 +0300

    Add project files.

---
+++ 1000_zadach_po_programmirovaniyu_SWITCH_CASE/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1000_zadach_po_programmirovaniyu_SWITCH_CASE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
